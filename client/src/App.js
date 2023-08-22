dc293eb9cd4770bfcd57d5f969fc7d5b60829a54
gitirom <wadiaromdane10@gmail.com>
2023-08-22 17:55:09 +0200

    I finish the Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcom to the new project</h1>
+    <div >
+      <Home />
     </div>
   );
 }
