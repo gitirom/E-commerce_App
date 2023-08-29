4d4d1da353951e78249cd854b476ca2960e4ef22
gitirom <wadiaromdane10@gmail.com>
2023-08-29 15:28:10 +0200

    I have finish creating Single Product Page Page compo..

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/Navbar";
 import Announcement from "../components/Announcement";
 import NewsLetter from "../components/NewsLetter";
 import Footer from "../components/Footer";
+import { Add, Remove } from "@material-ui/icons";
 
 const Container = styled.div`
 
@@ -16,19 +17,98 @@ const ImgContainer = styled.div`
 `;
 const Image = styled.img`
     width: 100%;
+    height: 90vh;
+    object-fit: cover;
 `;
 const InfoContainer = styled.div`
     flex: 1;
+    padding: 0px 50px;
 `;
 const Title = styled.h1`
-
+    font-weight: 200;
 `;
 const Desc = styled.p`
+    margin: 20px 0px;
+    text-align: justify;
 
 `;
 const Price = styled.span`
-
+    font-weight: 100;
+    font-size: 40px;
 `;
+const FilterContainer = styled.div`
+    width: 50%;
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+`;
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`;
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`;
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${(props) => props.color};
+    margin: 0px 5px;
+    cursor: pointer;
+`;
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+`;
+const FilterSizeOption = styled.option`
+`;
+const AddContainer = styled.div`
+    width: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`;
+
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0px 5px;
+`;
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+    border-radius: 2px;
+    transition: all 0.4s ease 0s;
+    
+    &:hover{
+        background-color: teal;
+        color: white;
+        letter-spacing: 1px;
+        -webkit-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+        -moz-box-shadow: 0px 5px 40px -10px rgba(0,0,0,0.57);
+        box-shadow: 5px 40px -10px rgba(0,0,0,0.57);
+        transition: all 0.4s ease 0s;
+    }
+`;
+
 
 const Product = () => {
     return (
@@ -37,7 +117,7 @@ const Product = () => {
             <Announcement />
             <Wrapper>
                 <ImgContainer>
-                <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+                <Image src="https://images.pexels.com/photos/8063381/pexels-photo-8063381.jpeg" />
                 </ImgContainer>
                 <InfoContainer>
                 <Title>Denim Jumpsuit</Title>
@@ -49,6 +129,32 @@ const Product = () => {
                     condimentum ac, volutpat ornare.
                 </Desc>
                 <Price>$ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="black" />
+                        <FilterColor color="darkblue" />
+                        <FilterColor color="gray" />
+                    </Filter>
+                    <Filter>
+                    <FilterTitle>Size</FilterTitle>
+                    <FilterSize>
+                        <FilterSizeOption>XS</FilterSizeOption>
+                        <FilterSizeOption>S</FilterSizeOption>
+                        <FilterSizeOption>M</FilterSizeOption>
+                        <FilterSizeOption>L</FilterSizeOption>
+                        <FilterSizeOption>XL</FilterSizeOption>
+                    </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <Remove style={{cursor: "pointer"}} />
+                        <Amount>1</Amount>
+                        <Add style={{cursor: "pointer"}} />
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
                 </InfoContainer>
             </Wrapper>
             <NewsLetter />
