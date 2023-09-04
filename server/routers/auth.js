f8d2168b721a63ec724c955ab5bc28535026e603
gitirom <wadiaromdane10@gmail.com>
2023-09-04 17:37:24 +0200

    I have finish creating Auth Routes

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,12 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 //REGISTER
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString(),
     });
 
     try {
@@ -16,6 +21,38 @@ router.post("/register", async (req, res) => {
         res.status(500).json(err);
     }
 
+});
+
+//LOGIN
+router.post("/login", async(req, res) => {
+    try {
+        const user = await User.findOne({email:req.body.email});
+        !user && res.status(401).json("Wrong password or username!");
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        originalPassword != req.body.password && res.status(401).json("Wrong Password!");
+
+        const accessToken = jwt.sign(
+            {   
+                id: user._id,
+                usAdmin: user.isAdmin
+            },
+                process.env.JWT_SEC,
+                { expiresIn:"7d" }
+            );
+
+        const { password, ...others } = user._doc;
+        res.status(200).json({ ...others, accessToken });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
